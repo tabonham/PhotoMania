58d4d777fcf2e9f203e586f3f6448e3e06cfe542
tabonham <tracyannbonham@gmail>
2022-01-05 15:59:34 -0600

    Update ContentView.swift

--- PhotoMania/ContentView.swift
+++ PhotoMania/ContentView.swift
@@ -7,10 +7,73 @@
 
 import SwiftUI
 
+class ViewModel: ObservableObject {
+    @Published var image: Image?
+    
+    func fetchNewImage() {
+        guard let url = URL(string: "https://random.imagecdn.app/500/500") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+            
+            DispatchQueue.main.async {
+                guard let uiImage = UIImage(data: data) else {
+                    return
+                }
+                self.image = Image(uiImage: uiImage)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
 struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                
+                Spacer()
+                
+                if let image = viewModel.image {
+                    ZStack {
+                        image
+                            .resizable()
+                            .foregroundColor(Color.pink)
+                            .frame(width: 200, height: 200)
+                            .padding()
+                    }
+                    .frame(width: UIScreen.main.bounds.width / 1.5,
+                           height: UIScreen.main.bounds.width / 1.5)
+                    .background(Color.pink)
+                    .cornerRadius(8)
+                } else {
+                Image(systemName: "photo")
+                    .resizable()
+                    .foregroundColor(Color.pink)
+                    .frame(width: 200, height: 200)
+                    .padding()
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    viewModel.fetchNewImage()
+                }, label: {
+                    Text("New Image!")
+                        .frame(width: 250, height: 55)
+                        .foregroundColor(Color.white)
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                        .padding()
+                })
+            }
+            .navigationTitle("Photo Mania")
+        }
     }
 }
 
@@ -19,3 +82,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
