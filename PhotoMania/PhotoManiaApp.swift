2f41617507540dc832b4e8a84270885f2459acf2
tabonham <tracyannbonham@gmail>
2022-01-05 12:33:53 -0600

    Initial Commit

---
+++ PhotoMania/PhotoManiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoManiaApp.swift
+//  PhotoMania
+//
+//  Created by Tracy Bonham on 1/5/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoManiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
